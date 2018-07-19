1cdc12b2614e165a7f162f8e7b6c4a3ba675fcf0
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-07-19 00:44:00 -0700

    updated twitter function

--- liri.js
+++ liri.js
@@ -25,20 +25,35 @@ switch (liri) {
         console.log("2. spotify 'any song name")
         console.log("3. movie 'any movie name")
         console.log("4. random")
-            
+
 }
 
 //functions 
 //twitter
 function tweets() {
-    let client = new twitter('./keys.twitter');
-    let twitterUser = process.argv[3]
-  
-  
+    let client = new twitter(keys.twitter);
+    //let twitterUser = process.argv[3]
+    let params = {
+        q: '@Jacob96691972',
+        count: 20
+    };
     
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error && response.statusCode ===200) {
+            
+            console.log('Last 20 Tweets:')
+            for (i = 0; i < tweets.length; i++) {
+                console.log('==========================================' + '\n' +
+                [i + 1] + '. ' + tweets[i].text +
+                '\nCreated on: ' + tweets[i].created_at)
+            }
+        } else {
+            console.log(error)
+        }
+    });
 }
 
- //spotify
+//spotify
 function spot() {
     let spotify = new Spotify(keys.spotify);
     console.log('spot')
@@ -46,23 +61,23 @@ function spot() {
 //movies
 function movie() {
     let movieRequest = process.argv[3]
-         if (!process.argv[3]) {
-             movieRequest = 'Mr. Nobody'
-             console.log("If you haven't watched 'Mr. Nobody', then you should: <http://www.imdb.com/title/tt0485947/>")
-             console.log("It's on Netflix!")
-         } 
-    request(`http://www.omdbapi.com/?t=${movieRequest}&y=&plot=short&apikey=922877d0`, function (error, response, body) {
+    if (!process.argv[3]) {
+        movieRequest = 'Mr. Nobody'
+        console.log("If you haven't watched 'Mr. Nobody', then you should: <https://www.imdb.com/title/tt0485947/>")
+        console.log("It's on Netflix!")
+    }
+    request(`https://www.omdbapi.com/?t=${movieRequest}&y=&plot=short&apikey=922877d0`, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log("==========================================")
-            console.log("Title: " + JSON.parse(body).Title)
-            console.log("Release Year: " + JSON.parse(body).Year)
-            console.log("IMDB rating : " + JSON.parse(body).imdbRating)
-            console.log("Rotten Tomatoe rating : " + JSON.parse(body).Ratings[1].Value)
-            console.log("Country flimed in: " + JSON.parse(body).Country)
-            console.log("Language : " + JSON.parse(body).Language)
-            console.log("Plot: " + JSON.parse(body).Plot)
-            console.log("Actors: " + JSON.parse(body).Actors)
-            console.log("==========================================")
+            console.log('==========================================' +
+                '\nTitle: ' + JSON.parse(body).Title +
+                '\nRelease Year: ' + JSON.parse(body).Year +
+                '\nIMDB rating : ' + JSON.parse(body).imdbRating +
+                '\nRotten Tomatoe rating : ' + JSON.parse(body).Ratings[1].Value +
+                '\nCountry flimed in: ' + JSON.parse(body).Country +
+                '\nLanguage : ' + JSON.parse(body).Language +
+                '\nPlot: ' + JSON.parse(body).Plot +
+                '\nActors: ' + JSON.parse(body).Actors +
+                '\n==========================================')
         }
     });
 }
